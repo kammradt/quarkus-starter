0232b83263fb1f351b13c89217af8414165c977d
Kammradt <vinicius.kammradt1@gmail.com>
2020-12-06 18:57:47 -0300

    fix: :bug: moves all annotations to AccountResource implementation

--- src/main/kotlin/org/kammradt/account/AccountResource.kt
+++ src/main/kotlin/org/kammradt/account/AccountResource.kt
@@ -1,27 +1,54 @@
 package org.kammradt.account
 
+import common.annotation.Owner
+import common.annotation.Status
 import org.kammradt.account.domain.Account
 import org.kammradt.account.dto.AccountCreationRequest
 import org.kammradt.account.dto.AccountLoginRequest
 import org.kammradt.account.dto.AccountResponse
 import org.kammradt.account.dto.TokenResponse
+import javax.annotation.security.RolesAllowed
 import javax.inject.Inject
+import javax.ws.rs.*
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
 
-class AccountResource : IAccountResource {
+
+@Path("accounts")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+class AccountResource {
 
     @Inject
     lateinit var service: AccountService
 
-    override fun create(accountCreationRequest: AccountCreationRequest): AccountResponse = service.create(accountCreationRequest).toResponse()
+    @POST
+    @Status(Response.Status.CREATED)
+    fun create(accountCreationRequest: AccountCreationRequest): AccountResponse =
+            service.create(accountCreationRequest).toResponse()
 
-    override fun update(id: Long, account: AccountCreationRequest): AccountResponse = service.update(id, account).toResponse()
+    @PUT
+    @Path("{id}")
+    fun update(@PathParam("id") id: Long, account: AccountCreationRequest): AccountResponse =
+            service.update(id, account).toResponse()
 
-    override fun delete(id: Long) = service.delete(id)
+    @DELETE
+    @Path("{id}")
+    @RolesAllowed("ADMIN")
+    fun delete(@PathParam("id") id: Long) = service.delete(id)
 
-    override fun findById(id: Long): AccountResponse = service.findById(id).toResponse()
+    @GET
+    @Path("{id}")
+    @RolesAllowed("ADMIN")
+    fun findById(@PathParam("id") id: Long): AccountResponse = service.findById(id).toResponse()
 
-    override fun findAll(): List<AccountResponse> = service.findAll().map(Account::toResponse)
+    @GET
+    @Owner
+    @RolesAllowed("ADMIN")
+    fun findAll(): List<AccountResponse> = service.findAll().map(Account::toResponse)
 
-    override fun login(account: AccountLoginRequest): TokenResponse = service.login(account)
+    @POST
+    @Path("login")
+    fun login(account: AccountLoginRequest): TokenResponse = service.login(account)
 
 }
--- src/main/kotlin/org/kammradt/account/IAccountResource.kt
+++ src/main/kotlin/org/kammradt/account/IAccountResource.kt
@@ -1,45 +1,28 @@
 package org.kammradt.account
 
-import common.annotation.Status
 import org.eclipse.microprofile.openapi.annotations.tags.Tag
 import org.kammradt.account.dto.AccountCreationRequest
 import org.kammradt.account.dto.AccountLoginRequest
 import org.kammradt.account.dto.AccountResponse
 import org.kammradt.account.dto.TokenResponse
-import javax.annotation.security.RolesAllowed
-import javax.ws.rs.*
-import javax.ws.rs.core.MediaType
-import javax.ws.rs.core.Response
 
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
-@Path("accounts")
 interface IAccountResource {
 
     @Tag(name = "Account creation", description = "Public endpoint to account creation")
-    @POST
-    @Status(Response.Status.CREATED)
     fun create(accountCreationRequest: AccountCreationRequest): AccountResponse
 
-    @PUT
-    @Path("{id}")
-    fun update(@PathParam("id") id: Long, account: AccountCreationRequest): AccountResponse
+    @Tag(name = "Account creation", description = "Public endpoint to account creation")
+    fun update(id: Long, account: AccountCreationRequest): AccountResponse
 
-    @DELETE
-    @Path("{id}")
-    @RolesAllowed("ADMIN")
-    fun delete(@PathParam("id") id: Long)
+    @Tag(name = "Account creation", description = "Public endpoint to account creation")
+    fun delete(id: Long)
 
-    @GET
-    @Path("{id}")
-    @RolesAllowed("ADMIN")
-    fun findById(@PathParam("id") id: Long): AccountResponse
+    @Tag(name = "Account creation", description = "Public endpoint to account creation")
+    fun findById(id: Long): AccountResponse
 
-    @GET
-    @RolesAllowed("ADMIN")
+    @Tag(name = "Account creation", description = "Public endpoint to account creation")
     fun findAll(): List<AccountResponse>
 
-    @POST
-    @Path("login")
+    @Tag(name = "Account creation", description = "Public endpoint to account creation")
     fun login(account: AccountLoginRequest): TokenResponse
 }
