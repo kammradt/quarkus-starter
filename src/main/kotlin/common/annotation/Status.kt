5f6a4d371e9231fa2775c754fb67dda07161888e
Kammradt <vinicius.kammradt1@gmail.com>
2020-12-06 18:57:11 -0300

    fix: :bug: fixes missing implementation on StatusFilter

--- src/main/kotlin/common/annotation/Status.kt
+++ src/main/kotlin/common/annotation/Status.kt
@@ -1,7 +1,9 @@
 package common.annotation
 
+import javax.ws.rs.NameBinding
 import javax.ws.rs.core.Response
 
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
+@NameBinding
 annotation class Status(val value: Response.Status)
--- src/main/kotlin/common/annotation/StatusFilter.kt
+++ src/main/kotlin/common/annotation/StatusFilter.kt
@@ -1,17 +1,18 @@
 package common.annotation
 
+import javax.ws.rs.container.ContainerRequestContext
 import javax.ws.rs.container.ContainerResponseContext
+import javax.ws.rs.container.ContainerResponseFilter
 import javax.ws.rs.core.Response
 import javax.ws.rs.ext.Provider
 
 @Provider
-class StatusFilter {
-    fun filter(ctx: ContainerResponseContext) {
-        if (ctx.status == Response.Status.OK.statusCode) {
-            for (annotation in ctx.entityAnnotations) {
-                if (annotation is Status) {
-                    ctx.status = annotation.value.statusCode
-                    break
+class StatusFilter : ContainerResponseFilter {
+    override fun filter(request: ContainerRequestContext, response: ContainerResponseContext) {
+        when (response.status) {
+            Response.Status.OK.statusCode -> {
+                when (val status = response.entityAnnotations.find { it is Status }) {
+                    is Status -> response.status = status.value.statusCode
                 }
             }
         }
